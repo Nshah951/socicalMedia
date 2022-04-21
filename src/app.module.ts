d3f65c4f36a38508e1954efb0b5475756500df51
Nishant Shah <nishant.s@simformsolutions.com>
2022-04-21 12:11:44 +0530

    Created routes for User details in controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { PostController } from './post/post.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, UserController, PostController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+
+@Controller('user')
+export class UserController {
+    @Get('/@:username')
+    getByUsername(@Param('username') username : string):string{
+        return `Get user by username ${username}`
+    }
+    @Get(':userID')
+    getbyUserID(@Param('userID') userID: string){
+        console.log(userID);
+        return `Get user by ID ${userID}`
+    }
+    @Post('/')
+    createNewUser(@Body() user: string){
+        return `username created ${user}`
+    }
+    @Patch('/:userID')
+    updateUser(@Param('userID') userID: string,@Body() updatedata:string){
+        return `Update at ${userID} of ${updatedata}`
+    }
+    @Put('/:userID/follow')
+    followUser(@Param('userID') userID: string){
+        return `Follow user ${userID}`
+    }
+    @Delete('/:userID/follow')
+    unfollowUser(@Param('userID') userID:string){
+        return `UnFollow user ${userID}`
+    }
+    @Get('/{userid}/followers')
+    getFollowersList(@Param('userID') userID:string){
+        return `list of followers`
+    }
+    @Get('/:userID/following')
+    getFollowingList(@Param('userID') userID:string){
+        return `list of following`
+    }
+}
